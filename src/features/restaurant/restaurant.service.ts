2176f9defe8e26c569e72b8eec00ccca15519d5a
Anas <anasmassnaoui@gmail.com>
2021-07-26 13:06:50 +0100

    feat: implementing get, create, update methods for the service

--- src/features/restaurant/restaurant.service.ts
+++ src/features/restaurant/restaurant.service.ts
@@ -3,7 +3,7 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User } from "../user/basic/user.schema";
 import { RestaurantFormDto } from "./restaurant.dto";
-import { Restaurant, RestaurantDocument } from "./restaurant.shema";
+import { Restaurant, RestaurantDocument } from "./restaurant.schema";
 
 
 
@@ -13,7 +13,7 @@ export class RestaurantService {
     constructor(@InjectModel(Restaurant.name) private restaurantModel: Model<RestaurantDocument>) {}
 
     async get(userId: String) : Promise<RestaurantFormDto> {
-       const restaurant = await this.restaurantModel.findOne({ owner: userId })
+       const restaurant = await this.restaurantModel.findOne({ owner: userId as unknown as User })
 
        return restaurant
     }
@@ -22,8 +22,10 @@ export class RestaurantService {
 
     }
 
-    async update() {
-
+    async update(userId: String, restaurantFormDto: RestaurantFormDto) : Promise<RestaurantFormDto> {
+        const restaurant = await this.restaurantModel.updateOne({ owner: userId as unknown as User }, restaurantFormDto)
+        
+        return this.get(userId)
     }
 
 }
