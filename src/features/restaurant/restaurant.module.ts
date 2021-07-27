651f7d857e1776c2aead7c1a8d95abba85fd510a
Anas <anasmassnaoui@gmail.com>
2021-07-27 12:24:06 +0100

    feat: create module for the restaurant

---
+++ src/features/restaurant/restaurant.module.ts
@@ -0,0 +1,18 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { JwtStrategy } from "src/modules/auth";
+import { RestaurantController } from "./restaurant.controller";
+import { Restaurant, RestaurantSchema } from "./restaurant.schema";
+import { RestaurantService } from "./restaurant.service";
+
+@Module({
+    imports: [
+        MongooseModule.forFeature([{
+          name: Restaurant.name,
+          schema: RestaurantSchema
+        }], 'dinning')
+    ],
+    controllers: [ RestaurantController ],
+    providers: [ RestaurantService, JwtStrategy ]
+})
+export class RestaurantModule { }
