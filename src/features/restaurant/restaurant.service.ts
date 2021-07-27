78c6c8f059ea1c31e58451faaa140c37bac15a34
Anas <anasmassnaoui@gmail.com>
2021-07-27 12:10:36 +0100

    feat: complete get, create, update methods for manage restaurant

--- src/features/restaurant/restaurant.service.ts
+++ src/features/restaurant/restaurant.service.ts
@@ -15,17 +15,39 @@ export class RestaurantService {
     async get(userId: String) : Promise<RestaurantFormDto> {
        const restaurant = await this.restaurantModel.findOne({ owner: userId as unknown as User })
 
-       return restaurant
+       return restaurant ? restaurant.toJSON() : this.create(userId)
     }
 
-    async create() {
+    async create(userId: String, restaurantFormDto?: RestaurantFormDto) : Promise<RestaurantFormDto>{
+        const restaurant = new this.restaurantModel({
+            owner: userId,
+            ...restaurantFormDto
+        })
 
+        await restaurant.save()
+
+        return restaurant.toJSON();
     }
 
     async update(userId: String, restaurantFormDto: RestaurantFormDto) : Promise<RestaurantFormDto> {
-        const restaurant = await this.restaurantModel.updateOne({ owner: userId as unknown as User }, restaurantFormDto)
-        
-        return this.get(userId)
+        const { timeRange={}, localization={}, ...rest } = restaurantFormDto
+        let set = {}
+
+        // partial update for localization
+        Object
+        .keys(localization)
+        .forEach(key => set[`localization.${key}`] = localization[key])
+        // partial update for timerange
+        Object
+        .keys(timeRange)
+        .forEach(key => set[`timeRange.${key}`] = timeRange[key])
+
+        const result = await this.restaurantModel.updateOne({ owner: userId as unknown as User }, {
+            ...rest,
+            $set: set
+        })
+
+        return result.n ? this.get(userId) : this.create(userId, restaurantFormDto)
     }
 
 }
