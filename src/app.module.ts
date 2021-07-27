26fe8acbca857277acd1208931726ac540fe48d5
Anas <anasmassnaoui@gmail.com>
2021-07-27 12:25:07 +0100

    feat: implement restaurant module to the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { RestaurantModule } from './features/restaurant/restaurant.module';
 import { BasicModule } from './features/user/basic/';
 import { AuthModule, JwtGuard, JwtStrategy } from './modules/auth';
 
@@ -8,6 +9,7 @@ import { AuthModule, JwtGuard, JwtStrategy } from './modules/auth';
   imports: [
     AuthModule,
     BasicModule,
+    RestaurantModule,
     MongooseModule.forRoot('mongodb+srv://dinning:dinning@cluster0.arsju.mongodb.net/dinning?retryWrites=true&w=majority', {
       connectionName: 'dinning',
       useNewUrlParser: true,
