9ac0b0ab008441f81b81b9f804c811a3bc0a8781
Anas <anasmassnaoui@gmail.com>
2021-07-27 12:23:04 +0100

    feat: allow the fields to be optionals, support nested verification

--- src/features/restaurant/restaurant.dto.ts
+++ src/features/restaurant/restaurant.dto.ts
@@ -1,52 +1,17 @@
-import { IsBoolean, IsDate, IsNumber, IsPhoneNumber, IsString } from "class-validator"
+import { Type } from "class-transformer"
+import { IsBoolean, IsDate, IsDefined, IsNumber, IsOptional, IsPhoneNumber, IsString, ValidateNested } from "class-validator"
 import { BaseEntity, Entity } from "src/libs/PropsCleanerNestInterceptor"
 
-
-@BaseEntity({ strict: true })
-export class RestaurantFormDto {
-    @IsString()
-    @Entity()
-    title: string
-    @IsPhoneNumber()
-    @Entity()
-    phoneNumber: string
-    @IsString()
-    @Entity()
-    subTitle: string
-    @IsString()
-    @Entity()
-    slogan: string
-    @IsString()
-    @Entity()
-    color: string
-    @Entity()
-    timeRange: TimeRange
-    @IsString()
-    @Entity()
-    cover: string
-    @IsString()
-    @Entity()
-    description: string
-    @IsBoolean()
-    @Entity()
-    onTheSpot: boolean
-    @IsBoolean()
-    @Entity()
-    delivery: boolean
-    @IsBoolean()
-    @Entity()
-    takeAway: boolean
-    @Entity()
-    localization: Localization
-}
 
 @BaseEntity({ strict: true })
 export class TimeRange {
     @IsDate()
     @Entity()
+    @IsOptional()
     begin: Date
     @IsDate()
     @Entity()
+    @IsOptional()
     end: Date
 }
 
@@ -54,11 +19,66 @@ export class TimeRange {
 export class Localization {
     @IsNumber()
     @Entity()
+    @IsOptional()
     lat: Number
     @IsNumber()
     @Entity()
+    @IsOptional()
     lon: Number
     @IsString()
     @Entity()
+    @IsOptional()
     address: String
+}
+
+@BaseEntity({ strict: true })
+export class RestaurantFormDto {
+    @IsString()
+    @Entity()
+    @IsOptional()
+    title: string
+    @IsPhoneNumber()
+    @Entity()
+    @IsOptional()
+    phoneNumber: string
+    @IsString()
+    @Entity()
+    @IsOptional()
+    subTitle: string
+    @IsString()
+    @Entity()
+    @IsOptional()
+    slogan: string
+    @IsString()
+    @Entity()
+    @IsOptional()
+    color: string
+    @Entity()
+    @ValidateNested({ each: true })
+    @Type(() => TimeRange)
+    timeRange: TimeRange
+    @IsString()
+    @Entity()
+    @IsOptional()
+    cover: string
+    @IsString()
+    @Entity()
+    @IsOptional()
+    description: string
+    @IsBoolean()
+    @Entity()
+    @IsOptional()
+    onTheSpot: boolean
+    @IsBoolean()
+    @Entity()
+    @IsOptional()
+    delivery: boolean
+    @IsBoolean()
+    @Entity()
+    @IsOptional()
+    takeAway: boolean
+    @Entity()
+    @ValidateNested({ each: true })
+    @Type(() => Localization)
+    localization: Localization
 }
