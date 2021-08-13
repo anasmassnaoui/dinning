b422b27105a95dba825e54ae4469c09c9b998dd0
Anas MASSNAOUI <anasmassnaoui@gmail.com>
2021-08-13 20:17:10 +0100

    import users_managment module

--- src/features/restaurant/restaurant.module.ts
+++ src/features/restaurant/restaurant.module.ts
@@ -4,15 +4,17 @@ import { JwtStrategy } from "src/modules/auth";
 import { RestaurantController } from "./restaurant.controller";
 import { Restaurant, RestaurantSchema } from "./restaurant.schema";
 import { RestaurantService } from "./restaurant.service";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-    imports: [
-        MongooseModule.forFeature([{
-          name: Restaurant.name,
-          schema: RestaurantSchema
-        }], 'dinning')
-    ],
-    controllers: [ RestaurantController ],
-    providers: [ RestaurantService, JwtStrategy ]
+  imports: [
+    MongooseModule.forFeature([{
+      name: Restaurant.name,
+      schema: RestaurantSchema
+    }], 'dinning'),
+    UsersModule
+  ],
+  controllers: [RestaurantController],
+  providers: [RestaurantService, JwtStrategy]
 })
 export class RestaurantModule { }
