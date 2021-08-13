8104c5c2b6d6f3254a6a86974dad873260fc1dda
Anas MASSNAOUI <anasmassnaoui@gmail.com>
2021-08-13 20:22:46 +0100

    provide user service, auto indentation

--- src/features/user/basic/basic.module.ts
+++ src/features/user/basic/basic.module.ts
@@ -4,15 +4,16 @@ import { JwtStrategy } from "src/modules/auth";
 import { BasicController } from "./basic.controller";
 import { BasicService } from "./basic.service";
 import { User, UserSchema } from "./user.schema";
+import { UserService } from "./user.service";
 
 @Module({
-    imports: [
-        MongooseModule.forFeature([{
-          name: User.name,
-          schema: UserSchema
-        }], 'dinning')
-    ],
-    controllers: [ BasicController ],
-    providers:[ BasicService, JwtStrategy ],
+  imports: [
+    MongooseModule.forFeature([{
+      name: User.name,
+      schema: UserSchema
+    }], 'dinning')
+  ],
+  controllers: [BasicController],
+  providers: [BasicService, UserService, JwtStrategy],
 })
-export class BasicModule {}
+export class BasicModule { }
